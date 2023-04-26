e32327295cc338d1f6946711fa5df0622009ad06
Felipe Camilo Soares <fc.soares@live.com>
2023-04-26 10:54:00 -0300

    Create 1008.cpp

---
+++ grafos/1008.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int n, h;
+    double s;
+    cin >> n >> h >> s;
+    
+    printf("NUMBER = %d\nSALARY = U$ %.2f\n", n, s*h);
+ 
+    return 0;
+}
